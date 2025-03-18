9028ae08b3e41fe29491f7b5f5da206b6e83904a
bhavesh <bhmahajan055@gmail.com>
2025-03-18 11:57:44 -0700

    fixed eureka error only in ui client services not visible

---
+++ auth-service/src/main/java/com/codeflowdb/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.codeflowdb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Allow all requests
+                );
+
+        return http.build();
+    }
+}
