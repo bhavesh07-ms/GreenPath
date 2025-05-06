2b2719f1bdd60b9110e9ecc79b19a5c9dcdcdd34
bhavesh <bhmahajan055@gmail.com>
2025-05-06 09:57:02 +0530

    client

---
+++ client/src/app/components/route-search/route-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-route-search',
+  imports: [],
+  templateUrl: './route-search.component.html',
+  styleUrl: './route-search.component.scss'
+})
+export class RouteSearchComponent {
+
+}
--- server/route-service/src/main/java/com/codeflowdb/config/WebSecurityConfig.java
+++ server/route-service/src/main/java/com/codeflowdb/config/WebSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebSecurityConfig {
@@ -25,6 +27,19 @@ public class WebSecurityConfig {
         return new ModelMapper();
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // apply to all endpoints
+                        .allowedOrigins("http://localhost:4200") // allow Angular dev server
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // if using cookies/session
+            }
+        };
+    }
 
 
 }
