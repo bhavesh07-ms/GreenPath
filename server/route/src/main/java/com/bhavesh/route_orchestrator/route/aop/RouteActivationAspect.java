5f942ae8fd0cb2e7945d2ed3e6acf704a238a868
bhaveshwt4509 <bhavesh.mahajan@wissen.com>
2025-12-22 12:34:20 +0530

    aspect changes

--- server/route/src/main/java/com/bhavesh/route_orchestrator/route/aop/RouteActivationAspect.java
+++ server/route/src/main/java/com/bhavesh/route_orchestrator/route/aop/RouteActivationAspect.java
@@ -3,11 +3,13 @@ package com.bhavesh.route_orchestrator.route.aop;
 import com.bhavesh.commonutils.commonutils.dto.RouteEvent;
 import com.bhavesh.route_orchestrator.route.dto.RouteResponseDto;
 import com.bhavesh.route_orchestrator.route.service.RouteEventPublisher;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.util.RouteMatcher;
+import java.util.*;
 
 @Aspect
 @Component
@@ -32,12 +34,6 @@ public class RouteActivationAspect {
             return;
         }
 
-        Object source = response.getMeta().get("source");
-
-        // 🚨 CRITICAL GUARD
-        if (!"computed".equals(source)) {
-            return;
-        }
 
         // Extract what Lambda / traffic monitoring needs
         RouteEvent event = buildEvent(response);
@@ -45,7 +41,33 @@ public class RouteActivationAspect {
         routeEventPublisher.publish(event);
     }
 
+    @SuppressWarnings("unchecked")
     private RouteEvent buildEvent(RouteResponseDto response) {
-        return RouteEvent.builder().build();
+
+        Map<String, Object> payload = (Map<String, Object>) response.getPayload();
+        if (payload == null) return null;
+
+        Map<String, Object> data = (Map<String, Object>) payload.get("data");
+        if (data == null) return null;
+
+        // ---- Extract path
+        List<String> path = (List<String>) data.get("path");
+        if (path == null || path.isEmpty()) return null;
+
+        // ---- Extract baseline time
+        int baselineTimeMs = path.size() * 120;
+
+        // ---- Build routeId (deterministic)
+        String routeId = DigestUtils.sha256Hex(String.join("->", path));
+
+        return RouteEvent.builder()
+                .eventType("ROUTE_ACTIVATED")
+                .routeId(routeId)
+                .userId("anonymous-user")   // dummy for now
+                .path(path)
+                .baselineTime(baselineTimeMs)
+                .threshold(20)              // default 20% degradation
+                .build();
     }
+
 }
